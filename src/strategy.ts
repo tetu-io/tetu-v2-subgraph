04bc1dfc3869e715d095e1161f22c2697ad3c055
belbix <7453635@gmail.com>
2022-06-29 23:10:58 +0300

    strategy handlers

--- src/strategy.ts
+++ src/strategy.ts
@@ -8,52 +8,101 @@ import {
   EmergencyExit,
   InvestAll,
   ManualClaim,
-  RevisionIncreased,
+  RevisionIncreased, Strategy,
   Upgraded,
   WithdrawAllFromPool,
   WithdrawAllToSplitter,
   WithdrawFromPool,
   WithdrawToSplitter
 } from "./types/templates/Strategy/Strategy";
+import {SplitterEntity, StrategyEntity, StrategyHistory} from "./types/schema";
+import {Address, BigInt} from "@graphprotocol/graph-ts";
 
-
-export function handleClaimed(event: Claimed): void {
-}
+// ***************************************************
+//                 STATE CHANGES
+// ***************************************************
 
 export function handleUpgraded(event: Upgraded): void {
+  const strategy = StrategyEntity.load(event.address.toHexString()) as StrategyEntity;
+  const implementations = strategy.implementations;
+  implementations.push(event.params.implementation.toHexString())
+  strategy.implementations = implementations;
+  strategy.save()
 }
 
 export function handleCompoundRatioChanged(event: CompoundRatioChanged): void {
-}
-
-export function handleContractInitialized(event: ContractInitialized): void {
-}
-
-export function handleDepositToPool(event: DepositToPool): void {
-}
-
-export function handleEmergencyExit(event: EmergencyExit): void {
-}
-
-export function handleInvestAll(event: InvestAll): void {
-}
-
-export function handleManualClaim(event: ManualClaim): void {
+  const strategy = StrategyEntity.load(event.address.toHexString()) as StrategyEntity;
+  const strategyCtr = Strategy.bind(event.address);
+  const compoundDenominator = strategyCtr.COMPOUND_DENOMINATOR();
+  strategy.compoundRatio = event.params.newValue.toBigDecimal().div(compoundDenominator.toBigDecimal());
+  strategy.save()
 }
 
 export function handleRevisionIncreased(event: RevisionIncreased): void {
+  const strategy = StrategyEntity.load(event.address.toHexString()) as StrategyEntity;
+  strategy.revision = event.params.value.toI32();
+  strategy.save()
+}
+
+// ***************************************************
+//                    ACTIONS
+// ***************************************************
+
+export function handleDepositToPool(event: DepositToPool): void {
+  // updateBalances(event.address.toHexString(), event.block.timestamp.toI32());
+}
+
+export function handleEmergencyExit(event: EmergencyExit): void {
+  updateBalances(event.address.toHexString(), event.block.timestamp.toI32());
+}
+
+export function handleInvestAll(event: InvestAll): void {
+  // updateBalances(event.address.toHexString(), event.block.timestamp.toI32());
 }
 
 export function handleWithdrawAllFromPool(event: WithdrawAllFromPool): void {
+  // updateBalances(event.address.toHexString(), event.block.timestamp.toI32());
 }
 
 export function handleWithdrawAllToSplitter(
   event: WithdrawAllToSplitter
 ): void {
+  updateBalances(event.address.toHexString(), event.block.timestamp.toI32());
 }
 
 export function handleWithdrawFromPool(event: WithdrawFromPool): void {
+  // updateBalances(event.address.toHexString(), event.block.timestamp.toI32());
 }
 
 export function handleWithdrawToSplitter(event: WithdrawToSplitter): void {
+  updateBalances(event.address.toHexString(), event.block.timestamp.toI32());
+}
+
+function updateBalances(
+  address: string,
+  time: i32
+): void {
+  const strategy = StrategyEntity.load(address) as StrategyEntity;
+  const strategyCtr = Strategy.bind(Address.fromString(address));
+  strategy.tvl = strategyCtr.totalAssets().toBigDecimal();
+  saveStrategyHistory(strategy, time);
+  strategy.save();
+}
+
+function saveStrategyHistory(
+  strategy: StrategyEntity,
+  // @ts-ignore
+  time: i32
+): void {
+  const h = new StrategyHistory(strategy.id + '_' + BigInt.fromI32(time).toString());
+  h.strategy = strategy.id;
+  h.time = time;
+
+  h.tvl = strategy.tvl;
+  h.profit = strategy.profit;
+  h.loss = strategy.loss;
+  h.apr = strategy.apr;
+  h.averageApr = strategy.averageApr;
+
+  h.save();
 }
